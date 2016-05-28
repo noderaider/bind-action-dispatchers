56264f6699eff8a4c0a7586b7af98a300ac8b558
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-05-28 04:47:58 -0700

    reset to full

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,7 @@
 import { bindActionCreators } from 'redux'
+import { assert } from 'chai'
+
+const IS_DEV = process.env.NODE_ENV !== 'production'
 
 /**
  * bindActionDispatchers - curries mapDispatchToProps with bindActionCreators to simplify React action dispatchers.
@@ -6,6 +9,12 @@ import { bindActionCreators } from 'redux'
  * @return {function(dispatch: function): actionDispatcher}               a dispatchable action that can be passed directly to mapDispatchToProps argument of connect.
  */
 export default function bindActionDispatchers(actionCreators) {
+  if(IS_DEV) {
+    assert(['object', 'function'].includes(typeof actionCreators))
+    assert.isNotArray(actionCreators)
+    assert.isNotNumber(actionCreators)
+    assert.isNotBoolean(actionCreators)
+  }
   if(typeof actionCreators === 'function')
     return (dispatch, ownProps) => bindActionCreators(actionCreators(ownProps), dispatch)
   return dispatch => bindActionCreators(actionCreators, dispatch)
