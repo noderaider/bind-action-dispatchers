92adfbf141c317db7763e34c028bacd6749fa847
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-05-28 02:38:45 -0700

    documented unit tests with ESDoc

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,7 +3,7 @@ import { bindActionCreators } from 'redux'
 /**
  * bindActionDispatchers - curries mapDispatchToProps with bindActionCreators to simplify React action dispatchers.
  * @param  {Object|function(ownProps: Object): Object} actionCreators - object with action creator properties or function taking ownProps and returning object with action creators.
- * @return {function(dispatch: function(action: Object))}               a dispatchable action that can be passed directly to mapDispatchToProps argument of connect.
+ * @return {function(dispatch: function): actionDispatcher}               a dispatchable action that can be passed directly to mapDispatchToProps argument of connect.
  */
 export default function bindActionDispatchers(actionCreators) {
   const mapDispatchToProps = typeof actionCreators === 'function' ? (dispatch, ownProps) => actionCreators(ownProps)
--- src/spec/tests/bindActionDispatchersSpec.js
+++ src/spec/tests/bindActionDispatchersSpec.js
@@ -1,4 +1,6 @@
 import { bindActionCreators } from 'redux'
+
+/** @test {bindActionDispatchers} */
 describe('bindActionDispatchers', () => {
   const bindActionDispatchers = require('../../lib').default
   const fooActionCreator = () => ({ type: 'FOO', payload: 'foo' })
@@ -6,13 +8,18 @@ describe('bindActionDispatchers', () => {
   const fooActionCreatorWithProps = ownProps => ({ type: 'FOO', payload: ownProps })
   const barActionCreatorWithProps = ownProps => ({ type: 'BAR', payload: ownProps })
 
+  /** @test {bindActionDispatchers} */
   it('is a function', () => expect(bindActionDispatchers).toEqual(jasmine.any(Function)))
+
+  /** @test {bindActionDispatchers} */
   it('is equivalent to bindActionCreators for object of functions input', () => {
     const actionCreators = { fooActionCreator, barActionCreator }
     const dispatch = () => {}
     expect(JSON.stringify(bindActionDispatchers(actionCreators)(dispatch)))
       .toEqual(JSON.stringify(bindActionCreators(actionCreators, dispatch)))
   })
+
+  /** @test {bindActionDispatchers} */
   it('is equivalent to bindActionCreators for ownProps input', () => {
     const actionCreators = ownProps =>  ({ fooActionCreator: fooActionCreatorWithProps(ownProps)
                                         , barActionCreator: barActionCreatorWithProps
