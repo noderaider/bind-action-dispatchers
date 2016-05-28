03d9d0f9799b6f073b94a20eb6cd0273cfba2894
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-05-28 03:29:41 -0700

    more unit tests, fixed major bug

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,7 @@
 import { bindActionCreators } from 'redux'
+import { assert } from 'chai'
+
+const IS_DEV = process.env.NODE_ENV !== 'production'
 
 /**
  * bindActionDispatchers - curries mapDispatchToProps with bindActionCreators to simplify React action dispatchers.
@@ -6,7 +9,14 @@ import { bindActionCreators } from 'redux'
  * @return {function(dispatch: function): actionDispatcher}               a dispatchable action that can be passed directly to mapDispatchToProps argument of connect.
  */
 export default function bindActionDispatchers(actionCreators) {
-  const mapDispatchToProps = typeof actionCreators === 'function' ? (dispatch, ownProps) => actionCreators(ownProps)
-                                                                  : actionCreators
-  return dispatch => bindActionCreators(mapDispatchToProps)
+  if(IS_DEV) {
+    assert(['object', 'function'].includes(typeof actionCreators))
+    assert.isNotArray(actionCreators)
+    assert.isNotNumber(actionCreators)
+    assert.isNotBoolean(actionCreators)
+  }
+
+  if(typeof actionCreators === 'function')
+    return (dispatch, ownProps) => actionCreators(ownProps)
+  return dispatch => bindActionCreators(actionCreators)
 }
--- src/spec/tests/bindActionDispatchersSpec.js
+++ src/spec/tests/bindActionDispatchersSpec.js
@@ -12,6 +12,21 @@ describe('bindActionDispatchers', () => {
   it('is a function', () => expect(bindActionDispatchers).toEqual(jasmine.any(Function)))
 
   /** @test {bindActionDispatchers} */
+  it('returns a function for object arg', () => expect(bindActionDispatchers({ FOO: 'BAR' })).toEqual(jasmine.any(Function)))
+
+  /** @test {bindActionDispatchers} */
+  it('returns a function for function arg', () => expect(bindActionDispatchers(ownProps => { foo: ownProps })).toEqual(jasmine.any(Function)))
+
+  /** @test {bindActionDispatchers} */
+  it('throws on array arg', () => expect(() => bindActionDispatchers([ 'FOO', 'BAR' ])).toThrow())
+
+  /** @test {bindActionDispatchers} */
+  it('throws on number arg', () => expect(() => bindActionDispatchers(1337)).toThrow())
+
+  /** @test {bindActionDispatchers} */
+  it('throws on boolean arg', () => expect(() => bindActionDispatchers(true)).toThrow())
+
+  /** @test {bindActionDispatchers} */
   it('is equivalent to bindActionCreators for object of functions input', () => {
     const actionCreators = { fooActionCreator, barActionCreator }
     const dispatch = () => {}
@@ -19,13 +34,34 @@ describe('bindActionDispatchers', () => {
       .toEqual(JSON.stringify(bindActionCreators(actionCreators, dispatch)))
   })
 
+
   /** @test {bindActionDispatchers} */
   it('is equivalent to bindActionCreators for ownProps input', () => {
     const actionCreators = ownProps =>  ({ fooActionCreator: fooActionCreatorWithProps(ownProps)
                                         , barActionCreator: barActionCreatorWithProps
                                         })
+    const props = { FOO: 'BAR' }
     const dispatch = () => {}
-    expect(JSON.stringify(bindActionDispatchers(actionCreators)(dispatch)))
-      .toEqual(JSON.stringify(bindActionCreators(actionCreators, dispatch)))
+    expect(JSON.stringify(bindActionDispatchers(actionCreators(props))(dispatch)))
+      .toEqual(JSON.stringify(bindActionCreators(actionCreators(props), dispatch)))
+  })
+
+  /** @test {bindActionDispatchers} */
+  xit('is equal to bindActionCreators for object of functions input', () => {
+    const actionCreators = { fooActionCreator, barActionCreator }
+    const dispatch = () => {}
+    expect(bindActionDispatchers(actionCreators)(dispatch))
+      .toEqual(bindActionCreators(actionCreators, dispatch))
+  })
+
+
+  /** @test {bindActionDispatchers} */
+  xit('is equal to bindActionCreators for ownProps input', () => {
+    const actionCreators = ownProps =>  ({ fooActionCreator: fooActionCreatorWithProps(ownProps)
+                                        , barActionCreator: barActionCreatorWithProps
+                                        })
+    const dispatch = () => {}
+    expect(bindActionDispatchers(actionCreators)(dispatch))
+      .toEqual(bindActionCreators(actionCreators, dispatch))
   })
 })
