ea1e6d872704f7a9617ee64c811c17891f5eabd5
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-05-28 02:33:02 -0700

    better esdoc and images

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,12 @@
 import { bindActionCreators } from 'redux'
 
-/** action creators can either be an object of action creators or a function accepting ownProps (using ownProps causes redux to run in a slower mode) */
+/**
+ * bindActionDispatchers - curries mapDispatchToProps with bindActionCreators to simplify React action dispatchers.
+ * @param  {Object|function(ownProps: Object): Object} actionCreators - object with action creator properties or function taking ownProps and returning object with action creators.
+ * @return {function(dispatch: function(action: Object))}               a dispatchable action that can be passed directly to mapDispatchToProps argument of connect.
+ */
 export default function bindActionDispatchers(actionCreators) {
-  const mapDispatchToProps = typeof actionCreators === 'function' ? (dispatch, ownProps) => actionCreators(ownProps) : actionCreators
+  const mapDispatchToProps = typeof actionCreators === 'function' ? (dispatch, ownProps) => actionCreators(ownProps)
+                                                                  : actionCreators
   return dispatch => bindActionCreators(mapDispatchToProps)
 }
